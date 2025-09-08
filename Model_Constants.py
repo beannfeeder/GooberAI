36dfa77155bcf172f1aa4e55efa54d1af5c5742f
Ng Yan Bin <yanbinngg027@gmail.com>
2025-09-08 12:49:54 +0800

    Revert to original state

---
+++ Model_Constants.py
@@ -0,0 +1,17 @@
+"""
+kind, no
+videoId, no
+publishedAt, maybe X
+channelId, no
+title, yes X
+description, yes X
+tags, yes X
+defaultLanguage, yes X
+defaultAudioLanguage, yes X
+contentDuration, yes X
+viewCount, yes Y
+likeCount, yes Y
+favouriteCount, yes Y
+commentCount, yes Y
+topicCategories maybe X
+"""
